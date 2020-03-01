587bfc0d34ce9e83e636c5d175e0bdd4eaae1167
Arter Tendean <50015208+S11720037@users.noreply.github.com>
2020-03-01 18:33:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
+'''
+Arter Tendean
+01-03-2020
+
+Program untuk mengambil data dari imdb dan rottentomatoes
+'''
+
+
 from bs4 import BeautifulSoup
+from rotten_tomatoes_client import RottenTomatoesClient
 import requests
 import pandas as pd
 
-
-url = 'https://www.imdb.com/chart/toptv/?ref_=nv_mv_250'
-# url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
-
-result = requests.get(url)
-c = result.content
-
-soup = BeautifulSoup(c, 'lxml')
-
-summary = soup.find('div', {'class' : 'article'})
-
-
+#membuat list untuk menyimpan data
 judulFilm = []
 tahunFilm = []
 skorFilm_imdb = []
@@ -23,100 +21,132 @@ kategoriFilm = []
 deskripsiFilm = []
 pemeranFilm = []
 linkFilm = []
+skorFilm_tomatoes = []
+skorFilmDariPengguna_tomatoes = []
+jumlahPengguna_tomatoes = []
+nomor = []
 
 
+#imdb
+def imdb(url):
+  result = requests.get(url)
+  sourceCode = result.content
+  soup = BeautifulSoup(sourceCode, 'lxml')
+  summary = soup.find('div', {'class' : 'article'})
 
-counter = 0
-for row in soup.find('table').findAll('tr'):
-  
-  #mengambil data tahun film
-  for i in row.findAll('span', {'class' : 'secondaryInfo'}):
-    tahunFilm.append(i.text[1:-1])
-
-  #mengambil nama aktor
-  for i in row.findAll('td', {'class' : 'titleColumn'}):
-    pemeranFilm.append(i.a['title'])
-
-  #mengambil rating film
-  for i in row.findAll('strong'):
-    skorFilm_imdb.append(i.text)
+  for row in soup.find('table').findAll('tr'):
     
-  #mengambil judul film, link, deskripsi, kategori, durasi film, dan jumlah pengguna
-  for i in row.findAll('td', {'class' : 'titleColumn'}):
+    #mengambil data tahun film
+    for i in row.findAll('span', {'class' : 'secondaryInfo'}):
+      tahunFilm.append(i.text[1:-1])
 
-    for j in i.findAll('a', href=True):
+    #mengambil nama aktor
+    for i in row.findAll('td', {'class' : 'titleColumn'}):
+      pemeranFilm.append(i.a['title'])
 
-      #mengambil judul
-      judulFilm.append(j.text)
+    #mengambil rating film
+    for i in row.findAll('strong'):
+      skorFilm_imdb.append(i.text)
+      
+    #mengambil judul film, link, deskripsi, kategori, durasi film, dan jumlah pengguna
+    for i in row.findAll('td', {'class' : 'titleColumn'}):
+      for j in i.findAll('a', href=True):
+        
+        #mengambil judul
+        judulFilm.append(j.text)
 
-      #mengambil link
-      linkFilm.append('https://www.imdb.com/'+j['href'])
+        #mengambil link
+        linkFilm.append('https://www.imdb.com/'+j['href'])
 
-      #arter
-      raw = requests.get('https://www.imdb.com/'+j['href']).content
-      sourceCode = BeautifulSoup(raw, 'lxml')
+        #mengambil source code halaman
+        raw = requests.get('https://www.imdb.com/'+j['href']).content
+        sourceCode = BeautifulSoup(raw, 'lxml')
 
-      #mengambil deskripsi
-      deskripsi = sourceCode.find('div', {'class' : 'summary_text'})
-      deskripsiFilm.append(deskripsi.text.strip())
+        #mengambil deskripsi
+        deskripsi = sourceCode.find('div', {'class' : 'summary_text'})
+        deskripsiFilm.append(deskripsi.text.strip())
 
-      #mengambil kategori
-      data = sourceCode.find('div', {'class' : 'subtext'})
-      kategoriList = data.findAll('a')
+        #mengambil kategori
+        data = sourceCode.find('div', {'class' : 'subtext'})
+        kategoriList = data.findAll('a')
+        temp = ''
+        for k in range(0,len(kategoriList) - 1):
+          # print(kategoriList[k].text)
+          temp += kategoriList[k].text+ ', '
+        kategoriFilm.append(temp[:-2])
 
-      temp = ''
-      for k in range(0,len(kategoriList) - 1):
-        # print(kategoriList[k].text)
-         temp += kategoriList[k].text+ ', '
-      kategoriFilm.append(temp[:-2])
+        #mengambil durasi
+        try:
+          data = sourceCode.find('time')
+          durasiFilm.append(data.text.strip())
+          # print(data.text[25:-21])
+        except:
+          durasiFilm.append('-')
 
-      ##mengambil durasi
+        #mengambil jumlah pengguna yang memberikan rating
+        ratingList = sourceCode.find('span', {'class' : 'small'})
+        jumlahPengguna_imdb.append(ratingList.text)
+
+
+#rottentomatoes
+def tomatoes(daftarJudul):
+  global nomor
+
+  #mengambil link sesuai dengan judul film
+  links = []
+  for judul, index in zip(daftarJudul, range(1, len(daftarJudul) + 1)):
+    
+    nomor.append(index)#list untuk nomor film
+
+    #mengambil link rottentomatoes berdasarkan judul film dengan menggunakan API
+    result = RottenTomatoesClient.search(term=judul, limit=5)
+    try:
+      # print(i,result['tvSeries'][0]['url'])
+      links.append('https://www.rottentomatoes.com' + result['tvSeries'][0]['url'])
+    except:
+      # print(i,'-')
+      links.append('-')
+
+  #mengambil skor, skor rata-rata, dan jumlah pengguna
+  for link in links:
+    try:
+      data = requests.get(link)
+      data = BeautifulSoup(data.content, 'lxml')
+      score = data.findAll('span', {'class' : 'mop-ratings-wrap__percentage'})
+      users = data.findAll('strong', {'class' : 'mop-ratings-wrap__text--small'})
       try:
-        data = sourceCode.find('time')
-        durasiFilm.append(data.text.strip())
-        # print(data.text[25:-21])
+        # print(score[0].text.strip())
+        skorFilm_tomatoes.append(score[0].text.strip())
       except:
-        durasiFilm.append('-')
+        # print('-')
+        skorFilm_tomatoes.append('-')
+      try:
+        # print(score[1].text.strip())
+        skorFilmDariPengguna_tomatoes.append(score[1].text.strip())
+      except:
+        # print('-')
+        skorFilmDariPengguna_tomatoes.append('-')
+      try:
+        # print(users[1].text.strip())
+        jumlahPengguna_tomatoes.append(users[1].text.strip()[14:])
+      except:
+        # print('-')
+        jumlahPengguna_tomatoes.append('-')
+    except:
+      skorFilm_tomatoes.append('-')
+      skorFilmDariPengguna_tomatoes.append('-')
+      jumlahPengguna_tomatoes.append('-')
+      
 
-      #mengambil jumlah pengguna yang memberikan rating
-      ratingList = sourceCode.find('span', {'class' : 'small'})
-      jumlahPengguna_imdb.append(ratingList.text)
-
-
-  
-  counter += 1
-
-  # if counter == 11:
-  #   break
-
-
-# print(pemeranFilm)
-
-#mengambil deskripsi film #ini cuma test
-def ambilDeskripsi(url):
-
-  raw = requests.get(url).content
-
-  #mengambil deskripsi
-  sourceCode = BeautifulSoup(raw, 'lxml')
-
-
-# ambilDeskripsi('https://www.imdb.com/title/tt0988818/')
-# ambilDeskripsi('https://www.imdb.com/title/tt0903747/')
-
-# for i in range(5-1):
-#   print('Judul :', judulFilm[i])
-#   print('Tahun :', tahunFilm[i])
-#   print('Kategori :',kategoriFilm[i])
-#   print('Skor :',skorFilm_imdb[i],'dari',jumlahPengguna_imdb[i],'pengguna')
-#   print('Durasi :',durasiFilm[i])
-#   print('Deskripsi :',deskripsiFilm[i])
-#   print('Pemeran :',pemeranFilm[i])
-#   print('='*50)
+#memanggil fungsi untuk menjalankan program
+imdb('https://www.imdb.com/chart/toptv/?ref_=nv_mv_250')
+tomatoes(judulFilm)
 
 
 #menyimpan semua data yang didapatkan kedalam file dengan format csv
-dict = {'Judul' : judulFilm, 'Tahun' : tahunFilm, 'Durasi' : durasiFilm, 'Kategori' : kategoriFilm, 'Skor' : skorFilm_imdb, 'Pengguna ' : jumlahPengguna_imdb, 'Deskripsi' : deskripsiFilm, 'Pemeran' : pemeranFilm }
+dict = {'No' : nomor,
+        'Judul' : judulFilm, 'Tahun' : tahunFilm, 'Durasi' : durasiFilm, 'Kategori' : kategoriFilm, 'Skor IMDb' : skorFilm_imdb, 'Pengguna IMDb' : jumlahPengguna_imdb, 'Deskripsi' : deskripsiFilm, 'Pemeran' : pemeranFilm,
+        'Skor RottenTomatoes' : skorFilm_tomatoes, 'Skor RottenTomatoes Dari Pengguna' : skorFilmDariPengguna_tomatoes, 'Pengguna RottenTomatoes' : jumlahPengguna_tomatoes}
 arter = pd.DataFrame(dict)
 
 arter.to_csv('arter.csv', index=False)
